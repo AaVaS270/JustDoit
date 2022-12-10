1f26c3f2a5af9f0043fa5b6d0fb85e4449a2350a
Aavas Sedhai <asedhai@gmail.com>
2022-12-10 23:07:23 +0545

    Login and Signup

---
+++ lib/screens/Login/components/body.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_auth/screens/Login/components/background.dart';
+//
+// class Body extends StatelessWidget {
+//   const Body({
+//     Key? key,
+//
+//   }) : super(key: key);
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     Size size=MediaQuery.of(context).size;
+//     return Background(child: Column(
+//         mainAxisAlignment:MainAxisAlignment.center,
+//         children: <Widget>[
+//           Text("Login",style: TextStyle(fontWeight: FontWeight.bold),),
+//           SvgPicture.asset("assets/icons/login.svg",)
+//       ],
+//     ),);
+//   }
+// }
+//
